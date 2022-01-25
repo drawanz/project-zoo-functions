818e309076cdc2b76e89e72c2425ebd69629c92e
Leonidas <leonidas.drawanz@gmail.com>
2022-01-25 18:38:42 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = species.find((element) => element.name === animal).residents;
+  return animals.every((element) => element.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
