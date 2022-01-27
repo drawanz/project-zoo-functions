31811ad899cab18c451fc8e580122793b2b9dd67
Leonidas <leonidas.drawanz@gmail.com>
2022-01-27 16:30:01 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = data.employees.find((element) => element.id === id).responsibleFor[0];
+  const animalArray = data.species.find((element) => element.id === firstAnimal).residents;
+  const oldestAnimal = animalArray.sort((animalA, animalB) => animalB.age - animalA.age)[0];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
