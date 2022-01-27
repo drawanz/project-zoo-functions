5f5cfdf57b064b1331b02e9e154f8d6bc9b55500
Leonidas <leonidas.drawanz@gmail.com>
2022-01-27 19:17:05 -0300

    pedaço da 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function animalName(id) {
+  return data.species.find((element) => element.id === id).name;
 }
 
+function location(id) {
+  return data.species.find((element) => element.id === id).location;
+}
+
+function coverage(element) {
+  return {
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: element.responsibleFor.map((valorAtual) => animalName(valorAtual)),
+    locations: element.responsibleFor.map((valorAtual) => location(valorAtual)),
+  };
+}
+
+function getEmployeesCoverage() {
+  return data.employees.map((valorAtual) => coverage(valorAtual));
+}
+
+console.log(getEmployeesCoverage());
+// { name: 'Sharonda' }
 module.exports = getEmployeesCoverage;
